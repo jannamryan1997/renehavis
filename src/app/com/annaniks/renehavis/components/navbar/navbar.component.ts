21daf5687b5fdb0dc6167063ec978f19337bf7de
jannamryan1997 <jannamryan8@gmail.com>
2020-02-18 16:50:16 +0400

    creted scrip and href instagram page renehavis

--- src/app/com/annaniks/renehavis/components/navbar/navbar.component.ts
+++ src/app/com/annaniks/renehavis/components/navbar/navbar.component.ts
@@ -13,25 +13,26 @@ import { MenuItem } from '../../models/models';
 export class NavBarComponent implements OnInit {
 
     public showMenu: boolean;
-    public menuItem:MenuItem[] =
- [
-        {icon:"assets/images/md4.png", link: "О Продукте",  router: "/about-product" },
-        {icon:"assets/images/md3.png", link: "Аптеки", router: "/pharmacies" },
-        {icon:"assets/images/md1.png", link: "МЕРОПРИЯТИЯ", router: "/events" },
-        {icon:"assets/images/md5.png", link: "Полезные статьи",  router: "/articles" },
-        {icon:"assets/images/md2.png", link: "Инструкция", router: "/instruction" },
-        {icon:"assets/images/md6.png", link: "Контакты",  router: "/contacts" },
- 
-    ]
+    public menuItem: MenuItem[] =
+        [
+         
+            { icon: "assets/images/md4.png", link: "О Продукте", router: "/about-product" },
+            { icon: "assets/images/md3.png", link: "Аптеки", router: "/pharmacies" },
+            { icon: "assets/images/md1.png", link: "МЕРОПРИЯТИЯ", router: "/events" },
+            { icon: "assets/images/md5.png", link: "Полезные статьи", router: "/articles" },
+            { icon: "assets/images/md2.png", link: "Инструкция", router: "/instruction" },
+            { icon: "assets/images/md6.png", link: "Контакты", router: "/contacts" },
+
+        ]
 
 
     public visibleMenuItems: MenuItem[] = [];
 
-    constructor(public menuService: MenuService,private _router: Router) { }
+    constructor(public menuService: MenuService, private _router: Router) { }
 
     ngOnInit() {
-      
-        
+
+
     }
 
 
