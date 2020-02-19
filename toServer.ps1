4e224277dbf2e341708ac11c4383989deb3ab57b
jannamryan1997 <jannamryan8@gmail.com>
2020-02-19 11:32:58 +0400

    changed header

--- toServer.ps1
+++ toServer.ps1
@@ -1,7 +1,7 @@
-# $env:Path += ";C:\Program Files\nodejs\"
-# npm run build:ssr
-# pause
-# winscp.exe renehavis /keepuptodate "C:\Users\Janna\Desktop\renehavis\dist" /var/www/html/dist /defaults 
-# pause 
+$env:Path += ";C:\Program Files\nodejs\"
+npm run build:ssr
+pause
+winscp.exe renehavis /keepuptodate "C:\Users\Janna\Desktop\renehavis\dist" /var/www/html/dist /defaults 
+pause 
 plink -ssh root@91.210.168.136 -pw o5EBnhNC "sudo service nginx restart"
-# plink -ssh root@91.210.168.136 -pw o5EBnhNC "sudo service supervisord start"
+plink -ssh root@91.210.168.136 -pw o5EBnhNC "sudo service supervisord start"
